ca3ea33975250e962287a2c90446db0471d4bc16
chigari101 <chigari101@gmail.com>
2023-07-04 08:29:46 -0700

    added fixed files

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; 
+    const task = true;
 	// eslint-disable-line no-unused-vars
-    const task2 = false; 
+    const task2 = false;
 	// eslint-disable-line no-unused-vars
   }
 
